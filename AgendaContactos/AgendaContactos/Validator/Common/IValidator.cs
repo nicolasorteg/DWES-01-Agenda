c4ce17dc2f3902b6b56fdd67972d7d92d4778bbb
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-11 13:38:50 +0200

    add validator interface

---
+++ AgendaContactos/AgendaContactos/Validator/Common/IValidator.cs
@@ -0,0 +1,18 @@
+﻿using AgendaContactos.Errors.Common;
+using CSharpFunctionalExtensions;
+
+namespace AgendaContactos.Validator.Common;
+
+/// <summary>
+/// Contrato para la validación de entidades
+/// </summary>
+/// <typeparam name="T">Entidad a validar</typeparam>
+public interface IValidator<T> {
+    
+    /// <summary>
+    /// Valida una entidad
+    /// </summary>
+    /// <param name="entity">Entidad a validar</param>
+    /// <returns>La entidad si se valida correctamente o error de dominio si no</returns>
+    Result<T, DomainError> Validar(T entity);
+}

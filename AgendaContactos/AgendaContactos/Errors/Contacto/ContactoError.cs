19e9a7c36f243b392d3eefe6b8e69d37a0d6dcd0
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-11 04:28:25 +0200

    add http codes

--- AgendaContactos/AgendaContactos/Errors/Contacto/ContactoError.cs
+++ AgendaContactos/AgendaContactos/Errors/Contacto/ContactoError.cs
@@ -9,35 +9,35 @@ namespace AgendaContactos.Errors.Contacto;
 public abstract record ContactoError(string Message) : DomainError(Message) {
     
     /// <summary>
-    /// Error de Contacto no encontrado según ID
+    /// Error de Contacto no encontrado según ID (404)
     /// </summary>
     /// <param name="Id">Identificador único</param>
     public sealed record NotFoundById(int Id) 
         : ContactoError($"No se ha encontrado ningún Contacto con el identificador: {Id}");
     
     /// <summary>
-    /// Error de Contacto no encontrado según Alidas
+    /// Error de Contacto no encontrado según Alidas (404)
     /// </summary>
     /// <param name="Alias">Alias del Contacto</param>
     public sealed record NotFoundByAlias(string Alias) 
         : ContactoError($"No se ha encontrado ningún Contacto con el alias: {Alias}");
     
     /// <summary>
-    /// Error de validación en el Contacto
+    /// Error de validación en el Contacto (400)
     /// </summary>
     /// <param name="Errores">Listado de mensajes de errores de validación</param>
     public sealed record Validation(IEnumerable<string> Errores) 
         : ContactoError($"Se han detectado errores de validación en la entidad:{Environment.NewLine} - {string.Join($"{Environment.NewLine}• ", Errores)}");
     
     /// <summary>
-    /// Error de Telefono repetido
+    /// Error de Telefono repetido (409)
     /// </summary>
     /// <param name="Telefono">Teléfono del contacto</param>
     public sealed record TelefonosRepetidos(string Telefono) 
         : ContactoError($"El Contacto con número {Telefono} ya existe en la agenda.");
     
     /// <summary>
-    /// Error de Alias repetido
+    /// Error de Alias repetido (409)
     /// </summary>
     /// <param name="Alias">Alias del contacto</param>
     public sealed record AliasRepetidos(string Alias) 

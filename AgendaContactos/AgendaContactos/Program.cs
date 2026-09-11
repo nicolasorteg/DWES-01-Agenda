10a87e3889d95125ab96e94f7407a743d12e40a8
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-11 18:43:44 +0200

    add json settings

--- AgendaContactos/AgendaContactos/Program.cs
+++ AgendaContactos/AgendaContactos/Program.cs
@@ -1,6 +1,1 @@
-﻿using System.Text;
-using Serilog;
-
-Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("Logs/log.txt").CreateLogger();
-Console.Title = "Agenda de Contactos";
-Console.OutputEncoding = Encoding.UTF8;
+﻿Console.Write("ejecución de prueba");

9b751266015423b07662e78e563b834df019fd83
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-11 03:37:59 +0200

    add contacto model

---
+++ AgendaContactos/AgendaContactos/Model/Contacto.cs
@@ -0,0 +1,14 @@
+﻿namespace AgendaContactos.Model;
+
+/// <summary>
+/// Modelo de Contacto inmutable
+/// </summary>
+public record Contacto {
+    
+    public int Id { get; init; }
+    
+    public string Nombre { get; init; } = string.Empty;
+    public string Telefono { get; init; } = string.Empty; // mejor que int para internacionalizar
+    public string Alias { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+}

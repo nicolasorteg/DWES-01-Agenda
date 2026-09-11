0edf82fff9a122b979cf3baa1294192ea6e6839d
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-11 13:36:28 +0200

    add logger config

--- AgendaContactos/AgendaContactos/Program.cs
+++ AgendaContactos/AgendaContactos/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("Logs/log.txt").CreateLogger();
+Console.Title = "Agenda de Contactos";
+Console.OutputEncoding = Encoding.UTF8;

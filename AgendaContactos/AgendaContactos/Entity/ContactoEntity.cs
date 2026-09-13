dee53489d7f8d74d178beeef0f4bea9cfcc5ea46
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-13 03:31:57 +0200

    add contacto entity

---
+++ AgendaContactos/AgendaContactos/Entity/ContactoEntity.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AgendaContactos.Entity;
+
+/// <summary>
+/// Representación de Contacto para la BD
+/// </summary>
+[Table("Contactos")]
+public class ContactoEntity {
+    
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // autoincrement
+    public int Id { get; set; }
+
+    [Required, MaxLength(14)]
+    public string Nombre { get; set; } = string.Empty;
+
+    [Required]
+    public string Telefono { get; set; } = string.Empty;
+
+    [Required, MaxLength(19)]
+    public string Alias { get; set; } = string.Empty;
+
+    [Required]
+    public string Email { get; set; } = string.Empty;
+}
--- AgendaContactos/AgendaContactos/Factories/ContactoFactory.cs
+++ AgendaContactos/AgendaContactos/Factories/ContactoFactory.cs
@@ -4,26 +4,26 @@ namespace AgendaContactos.Factories;
 
 public class ContactoFactory {
     
-    /// /// <summary>
+    /// <summary>
     /// Semilla de datos iniciales
     /// </summary>
     /// <returns>Contactos Iniciales</returns>
     public static IEnumerable<Contacto> Seed() =>
         new List<Contacto> {
-            new() { Id = 1, Nombre = "Nicolas", Telefono = "677888999", Alias = "nick" },
-            new() { Id = 2, Nombre = "Aitor", Telefono = "654789102", Alias = "aitoraros" },
-            new() { Id = 3, Nombre = "Jose", Telefono = "612345678", Alias = "Joselu" },
-            new() { Id = 4, Nombre = "Pepe", Telefono = "675849321", Alias = "Calvo" },
-            new() { Id = 5, Nombre = "Marcos", Telefono = "654389102", Alias = "Marquitos" },
-            new() { Id = 6, Nombre = "Antoine", Telefono = "689888754", Alias = "Antu" },
-            new() { Id = 7, Nombre = "Lucia", Telefono = "666662135", Alias = "Lu" },
-            new() { Id = 8, Nombre = "Laura", Telefono = "684930009", Alias = "" },
-            new() { Id = 9, Nombre = "Natalia", Telefono = "677444788", Alias = "Debi" },
-            new() { Id = 10, Nombre = "Diego", Telefono = "684012345", Alias = "Diegox" },
-            new() { Id = 11, Nombre = "Jesus", Telefono = "675444908", Alias = "Yisus" },
-            new() { Id = 12, Nombre = "Rafa", Telefono = "689078921", Alias = "Farra" },
-            new() { Id = 13, Nombre = "Carlos", Telefono = "674543987", Alias = "Carlitos" },
-            new() { Id = 14, Nombre = "Pablo", Telefono = "677778856", Alias = "Pablito" },
-            new() { Id = 15, Nombre = "Kevin", Telefono = "671253467", Alias = "Kev" },
+            new() { Id = 1, Nombre = "Nicolas", Telefono = "677888999", Alias = "Nick", Email = "Nicolas@gmail.com" },
+            new() { Id = 2, Nombre = "Aitor", Telefono = "654789102", Alias = "Aitoraros", Email = "Aitor@gmail.com" },
+            new() { Id = 3, Nombre = "Jose", Telefono = "612345678", Alias = "Joselu", Email = "Jose@gmail.com" },
+            new() { Id = 4, Nombre = "Pepe", Telefono = "675849321", Alias = "Calvo", Email = "Pepe@gmail.com" },
+            new() { Id = 5, Nombre = "Marcos", Telefono = "654389102", Alias = "Marquitos", Email = "Marcos@gmail.com" },
+            new() { Id = 6, Nombre = "Antoine", Telefono = "689888754", Alias = "Antu", Email = "Antoine@gmail.com" },
+            new() { Id = 7, Nombre = "Lucia", Telefono = "666662135", Alias = "Lu", Email = "Lucia@gmail.com" },
+            new() { Id = 8, Nombre = "Laura", Telefono = "684930009", Alias = "Lauraa", Email = "Laura@gmail.com" },
+            new() { Id = 9, Nombre = "Natalia", Telefono = "677444788", Alias = "Debi", Email = "Natalia@gmail.com" },
+            new() { Id = 10, Nombre = "Diego", Telefono = "684012345", Alias = "Diegox", Email = "Diego@gmail.com" },
+            new() { Id = 11, Nombre = "Jesus", Telefono = "675444908", Alias = "Yisus", Email = "Jesus@gmail.com" },
+            new() { Id = 12, Nombre = "Rafa", Telefono = "689078921", Alias = "Farra", Email = "Rafa@gmail.com" },
+            new() { Id = 13, Nombre = "Carlos", Telefono = "674543987", Alias = "Carlitos", Email = "Carlos@gmail.com" },
+            new() { Id = 14, Nombre = "Pablo", Telefono = "677778856", Alias = "Pablito", Email = "Pablo@gmail.com" },
+            new() { Id = 15, Nombre = "Kevin", Telefono = "671253467", Alias = "Kev", Email = "Kevin@gmail.com" },
         };
 }

2159c5b92768bf5435d2d6af2fdca938a9707f58
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-13 03:55:26 +0200

    add contacto mapper

---
+++ AgendaContactos/AgendaContactos/Mapper/ContactoMapper.cs
@@ -0,0 +1,31 @@
+﻿using AgendaContactos.Entity;
+using AgendaContactos.Model;
+
+namespace AgendaContactos.Mapper;
+
+public static class ContactoMapper {
+
+    /// <summary> Conversión Model -> Entity </summary>
+    public static ContactoEntity ToEntity(this Contacto contacto) => new ContactoEntity() {
+        Id = contacto.Id,
+        Nombre = contacto.Nombre,
+        Telefono = contacto.Telefono,
+        Alias = contacto.Alias,
+        Email = contacto.Email
+    };
+    
+    /// <summary> Conversión Entity -> Model </summary>
+    public static Contacto ToModel(this ContactoEntity entity) => new() {
+        Id = entity.Id,
+        Nombre = entity.Nombre,
+        Telefono = entity.Telefono,
+        Alias = entity.Alias,
+        Email = entity.Email
+    };
+    
+    /// <summary> Conversión Listado de Entity -> Listado de Model </summary>
+    public static IEnumerable<Contacto> ToModel(this IEnumerable<ContactoEntity> entities) {
+        return entities
+            .Select(c => c.ToModel());
+    }
+}

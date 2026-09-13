a705f9ff743293f2e981c9fb0bc67a16f1582e04
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-13 20:26:35 +0200

    add restricciones de telefono y alias

--- AgendaContactos/AgendaContactos/Entity/AppDbContext.cs
+++ AgendaContactos/AgendaContactos/Entity/AppDbContext.cs
@@ -1,9 +1,17 @@
-﻿using AgendaContactos.Model;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace AgendaContactos.Entity;
 
 /// <summary> Puente que conecta C# con SQLite para efcore </summary>
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options) {
+    
     public DbSet<ContactoEntity> Contactos => Set<ContactoEntity>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+        modelBuilder.Entity<ContactoEntity>(e => {
+            e.HasIndex(c => c.Telefono).IsUnique();
+            e.HasIndex(c => c.Alias).IsUnique();
+        });
+    }
 }
---
+++ AgendaContactos/AgendaContactos/Repository/EfCore/ContactoEfCoreRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace AgendaContactos.Repository.EfCore;
+
+public class ContactoEfCoreRepository {
+    
+}

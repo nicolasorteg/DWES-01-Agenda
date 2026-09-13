ab8af5f558b6b921626a04af508ee5a5c9551678
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-13 03:44:44 +0200

    add appdbcontext

---
+++ AgendaContactos/AgendaContactos/Entity/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using AgendaContactos.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaContactos.Entity;
+
+/// <summary> Puente que conecta C# con SQLite para efcore </summary>
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options) {
+    public DbSet<ContactoEntity> Contactos => Set<ContactoEntity>();
+}

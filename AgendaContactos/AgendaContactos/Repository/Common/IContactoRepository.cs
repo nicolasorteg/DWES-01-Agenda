691639b45b23093e69a6ecd8bf60bf90b76e76d1
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-13 19:49:44 +0200

    add contacto repository interface

---
+++ AgendaContactos/AgendaContactos/Repository/Common/IContactoRepository.cs
@@ -0,0 +1,11 @@
+﻿using AgendaContactos.Model;
+
+namespace AgendaContactos.Repository.Common;
+
+public interface IContactoRepository : ICrudRepository<Contacto, int> {
+    
+    /// <summary>
+    /// Obtiene una entidad por su Alias
+    /// </summary>
+    Contacto? GetByAlias(string alias);
+}

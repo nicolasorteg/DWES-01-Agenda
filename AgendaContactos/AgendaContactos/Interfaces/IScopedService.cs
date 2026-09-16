507006ac282a519a5df286f0cf5b4ac1d5ef1a21
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-16 22:29:24 +0200

    add interfaces marcadoras para ID con scrutor

---
+++ AgendaContactos/AgendaContactos/Interfaces/IScopedService.cs
@@ -0,0 +1,7 @@
+﻿namespace AgendaContactos.Interfaces;
+
+/// <summary>
+/// Interfaz marcadora para servicios con duración Scoped.
+/// Los servicios Scoped se crean una instancia por petición HTTP.
+/// </summary>
+public interface IScopedService;
---
+++ AgendaContactos/AgendaContactos/Interfaces/ITransientService.cs
@@ -0,0 +1,7 @@
+﻿namespace AgendaContactos.Interfaces;
+
+/// <summary>
+/// Interfaz marcadora para servicios con duración Transient.
+/// Los servicios Transient se crean una nueva instancia cada vez que se solicitan.
+/// </summary>
+public interface ITransientService;
